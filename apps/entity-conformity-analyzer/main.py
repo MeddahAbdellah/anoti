689b00a0252268dd821bb5eebde209fc4d972ca2
Abdallah MEDDAH <meddah.abdellah.spcx@gmail.com>
2024-01-03 22:01:00 +0100

    ✨ Add conformity analyzer

--- apps/entity-conformity-analyzer/main.py
+++ apps/entity-conformity-analyzer/main.py
@@ -2,12 +2,79 @@ import spacy
 from jurisAnalyze import jurisAnalyze
 from erepAnalyze import erepAnalyze
 import os
+from kafka import KafkaConsumer
+import json
+import time
+
+print("Conformity analyzer starting...")
 
 nlp = spacy.load("fr_core_news_lg")
+
+print("Loaded spacy model")
 
 directory_of_script = os.path.dirname(os.path.abspath(__file__))
 readyz_file_path = os.path.join(directory_of_script, 'readyz')
 
+print("Creating readiness file...")
+
 with open(readyz_file_path, "w") as file:
     file.write("ok")
 
+print("Created readiness file")
+
+kafka_sasl_password = os.getenv("KAFKA_SASL_PASSWORD")
+kafka_sasl_user = os.getenv("KAFKA_SASL_USER")
+kafka_service_dns = os.getenv("KAFKA_SERVICE_DNS")
+
+print("Creating consumer...")
+
+consumer = KafkaConsumer(
+    group_id="entity-conformity-analyzer",
+    bootstrap_servers=kafka_service_dns,
+    security_protocol='SASL_PLAINTEXT',
+    sasl_mechanism='PLAIN',
+    sasl_plain_username=kafka_sasl_user,
+    sasl_plain_password=kafka_sasl_password,
+    enable_auto_commit=True
+)
+
+print("Created consumer:")
+print(consumer)
+
+consumer.subscribe(["jurisprudence", "erep"])
+
+print("Subscribed to topics:")
+print(consumer.topics())
+
+for message in consumer:
+    print("Received message:")
+    print(message)
+    topic = message.topic
+
+    print("Topic:")
+    print(topic)
+
+    data = message.value.decode()
+    parts = data.split(', ')
+    companyName = parts[0].split(': ')[1]
+    risk = parts[1].split(': ')[1]
+
+    print("Analyzing message:")
+    print(companyName)
+    print(risk)
+    output = None
+
+    if topic == "jurisprudence":
+        output = jurisAnalyze(companyName, risk, nlp)
+        output_file = "juris.json"
+    elif topic == "erep":
+        output = erepAnalyze(companyName, risk)
+        output_file = "erep.json"
+
+    print("Finished analyzing")
+    if output is not None:
+        with open(output_file, "a") as file:
+            json.dump(output, file)
+            file.write("\n")
+
+    time.sleep(5)
--- apps/entity-conformity-endpoints/main.py
+++ apps/entity-conformity-endpoints/main.py
@@ -1,20 +1,92 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
 from typing import List
 import riskToArticle
+import os
+from kafka import KafkaProducer
+from kafka import KafkaProducer, KafkaAdminClient
+from kafka.admin import NewTopic, NewPartitions
+from kafka.errors import TopicAlreadyExistsError
+from pydantic import BaseModel
+
+kafka_sasl_password = os.getenv("KAFKA_SASL_PASSWORD")
+kafka_sasl_user = os.getenv("KAFKA_SASL_USER")
+kafka_service_dns = os.getenv("KAFKA_SERVICE_DNS")
+
+admin_client = KafkaAdminClient(
+    bootstrap_servers=kafka_service_dns,
+    security_protocol='SASL_PLAINTEXT',
+    sasl_mechanism='PLAIN',
+    sasl_plain_username=kafka_sasl_user,
+    sasl_plain_password=kafka_sasl_password
+)
+
+def create_or_update_topic(topic_name, num_partitions, replication_factor):
+    print(f"Checking if topic '{topic_name}' exists...")
+    existing_topics = admin_client.list_topics()
+    if topic_name in existing_topics:
+        print(f"Topic '{topic_name}' exists. Checking partitions...")
+        topic_metadata = admin_client.describe_topics([topic_name])
+        current_partitions = len(topic_metadata[0]['partitions'])
+        if current_partitions < num_partitions:
+            print(f"Updating topic '{topic_name}' to have {num_partitions} partitions...")
+            additional_partitions = NewPartitions(total_count=num_partitions)
+            admin_client.create_partitions(
+                topic_partitions={topic_name: additional_partitions},
+                validate_only=False
+            )
+            print(f"Partitions updated for topic '{topic_name}'.")
+        else:
+            print(f"No update needed. Topic '{topic_name}' already has the desired number of partitions.")
+    else:
+        try:
+            print(f"Creating topic '{topic_name}' with {num_partitions} partitions...")
+            topic_list = [NewTopic(name=topic_name, num_partitions=num_partitions, replication_factor=replication_factor)]
+            admin_client.create_topics(new_topics=topic_list, validate_only=False)
+            print(f"Topic '{topic_name}' created.")
+        except TopicAlreadyExistsError:
+            print(f"Topic '{topic_name}' already exists. No action taken.")
+
+
+
+print('Creating jurisprudence topic partitions...')
+create_or_update_topic("jurisprudence", num_partitions=10, replication_factor=1)
+
+print('Creating erep topic partitions...')
+create_or_update_topic("erep", num_partitions=10, replication_factor=1)
 
 app = FastAPI()
+
+producer = KafkaProducer(
+    bootstrap_servers=kafka_service_dns,
+    security_protocol='SASL_PLAINTEXT',
+    sasl_mechanism='PLAIN',
+    sasl_plain_username=kafka_sasl_user,
+    sasl_plain_password=kafka_sasl_password
+)
 
 @app.get("/risks")
 async def risks() -> List[str]:
     return list(riskToArticle.map.keys())
 
-@app.get("/jurisprudence")
-async def jurisprudence(text: str = Body(..., embed=True)) -> List[str]:
-    return text
+class JurisprudenceRequest(BaseModel):
+    companyName: str
+    risk: str
+
+@app.post("/jurisprudence")
+async def jurisprudence(request: JurisprudenceRequest) -> str:
+    message = f"Company: {request.companyName}, Risk: {request.risk}"
+    producer.send("jurisprudence", value=message.encode())
+    return "Message sent to the producer"
+
+class ErepRequest(BaseModel):
+    companyName: str
+    risk: str
 
 @app.get("/erep")
-async def erep(text: str = Body(..., embed=True)) -> List[str]:
-    return text
+async def erep(request: ErepRequest) -> str:
+    message = f"Company: {request.companyName}, Risk: {request.risk}"
+    producer.send("erep", value=message.encode())
+    return "Message sent to the producer"
 
 @app.get("/healthz")
 async def healthz() -> str:
